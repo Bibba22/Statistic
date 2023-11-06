494bfbfa74102975dc0b028fb0c50ae5c11580c7
Ivonia Fatima Viegas <127854815+Bibba22@users.noreply.github.com>
2023-11-05 19:58:57 -0800

    Update Controller.php

--- crud-laravel9/app/Http/Controllers/Controller.php
+++ crud-laravel9/app/Http/Controllers/Controller.php
@@ -1,13 +1,37 @@
 <?php
 
-namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+use App\Models\Data;
+use Illuminate\Http\Request;
+use App\Http\Controllers\DataTunggalController;
+use App\Http\Controllers\DatafrekwensiController;
+use App\Http\Controllers\DataDeskripsiController;
+use App\Http\Controllers\DatabergolongController;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
-{
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-}
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages/index');
+})->name("index");
+
+Route::get('/data_tunggal', [DataTunggalController::class, 'index'])->name("data_tunggal");
+Route::get('/data_tunggal/create', [DataTunggalController::class, 'create'])->name("data_tunggal.create");
+Route::post('/data_tunggal/store', [DataTunggalController::class, 'store'])->name("data_tunggal.store");
+Route::post('/data_tunggal/delete/{id}', [DataTunggalController::class, 'destroy'])->name("data_tunggal.destroy");
+Route::get('/data_tunggal/edit/{id}', [DataTunggalController::class, 'edit'])->name("data_tunggal.edit");
+
+Route::get('/data_frekuensi', [DataFrekwensiController::class, 'frekwensi'])->name("data_frekwensi");
+
+Route::get('/data_deskripsi', [DatadeskripsiController::class, 'getDataDeskripsiView'])->name("data_deskripsi");
+
+Route::get('/data_bergolong', [DataBergolongController::class, 'data_bergolong'])->name("data_bergolong");
